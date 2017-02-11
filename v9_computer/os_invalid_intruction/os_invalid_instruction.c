020c38d75fec021c6d88be14e047928ac405e7d5
chyyuu <yuchen@tsinghua.edu.cn>
2017-02-11 23:49:03 +0800

    add os_invalid_instruction

---
+++ v9_computer/os_invalid_intruction/os_invalid_instruction.c
@@ -0,0 +1,124 @@
+// os_invalid_instruction.c -- demo how to  capture invalid_instruction  exception/fault
+
+// instruction set
+enum {
+  HALT,ENT ,LEV ,JMP ,JMPI,JSR ,JSRA,LEA ,LEAG,CYC ,MCPY,MCMP,MCHR,MSET, // system & extended
+  LL  ,LLS ,LLH ,LLC ,LLB ,LLD ,LLF ,LG  ,LGS ,LGH ,LGC ,LGB ,LGD ,LGF , // load a
+  LX  ,LXS ,LXH ,LXC ,LXB ,LXD ,LXF ,LI  ,LHI ,LIF ,
+  LBL ,LBLS,LBLH,LBLC,LBLB,LBLD,LBLF,LBG ,LBGS,LBGH,LBGC,LBGB,LBGD,LBGF, // load b
+  LBX ,LBXS,LBXH,LBXC,LBXB,LBXD,LBXF,LBI ,LBHI,LBIF,LBA ,LBAD,
+  SL  ,SLH ,SLB ,SLD ,SLF ,SG  ,SGH ,SGB ,SGD ,SGF ,                     // store a
+  SX  ,SXH ,SXB ,SXD ,SXF ,
+  ADDF,SUBF,MULF,DIVF,                                                   // arithmetic
+  ADD ,ADDI,ADDL,SUB ,SUBI,SUBL,MUL ,MULI,MULL,DIV ,DIVI,DIVL,
+  DVU ,DVUI,DVUL,MOD ,MODI,MODL,MDU ,MDUI,MDUL,AND ,ANDI,ANDL,
+  OR  ,ORI ,ORL ,XOR ,XORI,XORL,SHL ,SHLI,SHLL,SHR ,SHRI,SHRL,
+  SRU ,SRUI,SRUL,EQ  ,EQF ,NE  ,NEF ,LT  ,LTU ,LTF ,GE  ,GEU ,GEF ,      // logical
+  BZ  ,BZF ,BNZ ,BNZF,BE  ,BEF ,BNE ,BNEF,BLT ,BLTU,BLTF,BGE ,BGEU,BGEF, // conditional
+  CID ,CUD ,CDI ,CDU ,                                                   // conversion
+  CLI ,STI ,RTI ,BIN ,BOUT,NOP ,SSP ,PSHA,PSHI,PSHF,PSHB,POPB,POPF,POPA, // misc
+  IVEC,PDIR,SPAG,TIME,LVAD,TRAP,LUSP,SUSP,LCL ,LCA ,PSHC,POPC,MSIZ,
+  PSHG,POPG,NET1,NET2,NET3,NET4,NET5,NET6,NET7,NET8,NET9,
+  POW ,ATN2,FABS,ATAN,LOG ,LOGT,EXP ,FLOR,CEIL,HYPO,SIN ,COS ,TAN ,ASIN, // math
+  ACOS,SINH,COSH,TANH,SQRT,FMOD,
+  IDLE                                                                   // do nothing
+};
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+
+// processor fault codes
+enum { 
+  FINST=4,  // illegal instruction
+};
+
+int current;
+
+int in(port)    { asm(LL,8); asm(BIN); }
+out(port, val)  { asm(LL,8); asm(LBL,16); asm(BOUT); }
+ivec(void *isr) { asm(LL,8); asm(IVEC); }
+lvadr()         { asm(LVAD); }
+stmr(int val)   { asm(LL,8); asm(TIME); }
+halt(value)     { asm(LL,8); asm(HALT); }
+
+void *memcpy() { asm(LL,8); asm(LBL, 16); asm(LCL,24); asm(MCPY); asm(LL,8); }
+void *memset() { asm(LL,8); asm(LBLB,16); asm(LCL,24); asm(MSET); asm(LL,8); }
+void *memchr() { asm(LL,8); asm(LBLB,16); asm(LCL,24); asm(MCHR); }
+
+write(fd, char *p, n) { while (n--) out(fd, *p++); }
+
+int strlen(char *s) { return memchr(s, 0, -1) - (void *)s; }
+
+enum { BUFSIZ = 32 };
+int vsprintf(char *s, char *f, va_list v)
+{
+  char *e = s, *p, c, fill, b[BUFSIZ];
+  int i, left, fmax, fmin, sign;
+
+  while (c = *f++) {
+    if (c != '%') { *e++ = c; continue; }
+    if (*f == '%') { *e++ = *f++; continue; }
+    if (left = (*f == '-')) f++;
+    fill = (*f == '0') ? *f++ : ' ';
+    fmin = sign = 0; fmax = BUFSIZ;
+    if (*f == '*') { fmin = va_arg(v,int); f++; } else while ('0' <= *f && *f <= '9') fmin = fmin * 10 + *f++ - '0';
+    if (*f == '.') { if (*++f == '*') { fmax = va_arg(v,int); f++; } else for (fmax = 0; '0' <= *f && *f <= '9'; fmax = fmax * 10 + *f++ - '0'); }
+    if (*f == 'l') f++;
+    switch (c = *f++) {
+    case 0: *e++ = '%'; *e = 0; return e - s;
+    case 'c': fill = ' '; i = (*(p = b) = va_arg(v,int)) ? 1 : 0; break;
+    case 's': fill = ' '; if (!(p = va_arg(v,char *))) p = "(null)"; if ((i = strlen(p)) > fmax) i = fmax; break;
+    case 'u': i = va_arg(v,int); goto c1;
+    case 'd': if ((i = va_arg(v,int)) < 0) { sign = 1; i = -i; } c1: p = b + BUFSIZ-1; do { *--p = ((uint)i % 10) + '0'; } while (i = (uint)i / 10); i = (b + BUFSIZ-1) - p; break;
+    case 'o': i = va_arg(v,int); p = b + BUFSIZ-1; do { *--p = (i & 7) + '0'; } while (i = (uint)i >> 3); i = (b + BUFSIZ-1) - p; break;
+    case 'p': fill = '0'; fmin = 8; c = 'x';
+    case 'x': case 'X': c -= 33; i = va_arg(v,int); p = b + BUFSIZ-1; do { *--p = (i & 15) + ((i & 15) > 9 ? c : '0'); } while (i = (uint)i >> 4); i = (b + BUFSIZ-1) - p; break;
+    default: *e++ = c; continue;
+    }
+    fmin -= i + sign;
+    if (sign && fill == '0') *e++ = '-';
+    if (!left && fmin > 0) { memset(e, fill, fmin); e += fmin; }
+    if (sign && fill == ' ') *e++ = '-';
+    memcpy(e, p, i); e += i;
+    if (left && fmin > 0) { memset(e, fill, fmin); e += fmin; }
+  }
+  *e = 0;
+  return e - s;
+}
+
+int printf(char *f) { static char buf[4096]; return write(1, buf, vsprintf(buf, f, &f)); } // XXX remove static from buf
+
+trap(int c, int b, int a, int fc, int pc)
+{
+  printf("TRAP: ");
+  switch (fc) {
+  case FINST:  printf("Find a invalid instrcution"); break;
+  default:     printf("other [%d]",fc); break;
+  }
+}
+
+alltraps()
+{
+  asm(PSHA);  asm(PSHB);  asm(PSHC);
+  trap();
+  asm(POPC);  asm(POPB);  asm(POPA);
+  asm(RTI);
+}
+
+main()
+{
+  int t, d; 
+  
+  current = 0;
+  ivec(alltraps);
+  
+  asm(STI);
+  
+  printf("test invalid instruction...\n");
+  // an invalid instruction "-1"
+  asm(-1);
+  printf("\n...ok\n");
+ 
+  halt(0);
+}
